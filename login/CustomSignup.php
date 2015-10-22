2f26db3fefd180a7a6349fddf73392ae625c49b6
sirromas <sirromas@ukr.net>
2015-10-22 07:05:58 +0300

    Some another fix

--- login/CustomSignup.php
+++ login/CustomSignup.php
@@ -4,38 +4,40 @@
  * @author sirromas
  * 
  */
-
 require ('../class.database.php');
 
 class CustomSignup
 {
 
     private $db;
+
     private $user;
+
     public $userid;
 
     function __construct($user)
     {
         print_r($user);
         echo "<br/>-------------------------<br/>";
-        $this->user = $user;        
-        $this->db = DB::getInstance();        
+        $this->user = $user;
+        $this->db = DB::getInstance();
         $this->userid = $this->getUserId();
     }
 
     function getUserId()
     {
-        $query = "select id from mdl_user where username='" . $this->user->email . "'";        
+        $query = "select id from mdl_user where username='" . $this->user->email . "'";
         $result = $this->db->query($query);
         while ($row = mysql_fetch_assoc($result)) {
             $userid = $row['id'];
         }
         return $userid;
     }
-    
-    function getContextLevel ($roleid) {
-        $query="select id from mdl_role_context_levels
-                wehere roleid='".$roleid."'";
+
+    function getContextLevel($roleid)
+    {
+        $query = "select id from mdl_role_context_levels
+                wehere roleid='" . $roleid . "'";
         $result = $this->db->query($query);
         while ($row = mysql_fetch_assoc($result)) {
             $contextlevel = $row['id'];
@@ -45,26 +47,45 @@ class CustomSignup
 
     function getCourseContext($courseid, $roleid)
     {
-        $contextlevel=$this->getContextLevel($roleid);
-        $query = "select id, contextlevel, instanceid from mdl_context
-                     where contextlevel='".$contextlevel."'
-                     and instanceid='" . $courseid . "'";        
-        $result = $this->db->query($query);        
+        $contextlevel = $this->getContextLevel($roleid);
+        $query = "select id from mdl_context
+                     where contextlevel='" . $contextlevel . "'
+                     and instanceid='" . $courseid . "'";
+        $result = $this->db->query($query);
         while ($row = mysql_fetch_assoc($result)) {
             $contextid = $row['id'];
-        }        
+        }
         return $contextid;
+    }
+
+    function getEnrolId($courseid)
+    {
+        $query = "select id from mdl_enrol
+                     where courseid=" . $courseid . " and enrol='self'";
+        while ($row = mysql_fetch_assoc($result)) {
+            $enrolid = $row['id'];
+        }
+        return $enrolid;
     }
 
     function assignRoles($userid, $courseid, $role)
     {
+        // Insert into mdl_role_assignments
         $roleid = ($role == 'student') ? 5 : 4;
+        $contextid = $this->getCourseContext($courseid, $roleid);
         $query = "insert into mdl_role_assignments
                     (roleid,contextid,userid,timemodified,modifierid,component)
-                    values(".$roleid.", 
-                            ".$this->getCourseContext($courseid,$roleid).",
-                            ".$this->userid.",
-                            ".time().", 2, ' ')";         
+                    values(" . $roleid . ", 
+                            " . $contextid . ",
+                            " . $this->userid . ",
+                            " . time() . ", 2, ' ')";
+        $result = $this->db->query($query);
+        // Insert itno mdl_user_enrolments
+        $enrolid = $this->getEnrolId($courseid);
+        $query = "Insert into mdl_user_enrolments
+                          (enrolid, userid)
+                  values (" . $enrolid . ",
+                           " . $userid . ")";
         $result = $this->db->query($query);
     }
 
@@ -127,13 +148,13 @@ class CustomSignup
 
     function processCourseRequest()
     {
-        if ($this->user->user_type == 'tutor') {                            
+        if ($this->user->user_type == 'tutor') {
             $this->assignRoles($this->userid, $this->user->course, $this->user->user_type);
-            //$this->createCourseGroups($this->user->course, $this->user->group);
-            //$this->addUserToGroups($this->userid, $this->user->course, 0, $this->user->user_type);
+            // $this->createCourseGroups($this->user->course, $this->user->group);
+            // $this->addUserToGroups($this->userid, $this->user->course, 0, $this->user->user_type);
         } else {
             $this->assignRoles($this->userid, $this->user->course, $this->user->user_type);
-            //$this->addUserToGroups($this->userid, $this->user->course, $this->user->group, $this->user->user_type);
+            // $this->addUserToGroups($this->userid, $this->user->course, $this->user->group, $this->user->user_type);
         }
     }
 }
